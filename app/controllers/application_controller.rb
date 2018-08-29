363af0357a0c7444b79ec992dc11925459fede0b
Jennifer Stoos <jstoos@bitlaw.com>
2018-08-29 13:16:37 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.new(:username => params[:username], :password => params[:password])
-    # user = User.find_by(:username => params[:username])
+    User.new(:username => params[:username], :password => params[:password])
+    user = User.find_by(:username => params[:username])
     if user
         redirect "/login"
     else
